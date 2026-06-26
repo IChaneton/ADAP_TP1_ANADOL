aeb2cea2675939a845cf61654941fc8685d58d65
Ignacio Chaneton <chata@MacBook-Pro-de-Ignacio.local>
2026-06-26 12:09:16 -0300

    correc bibliog

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const MAPA_NAVEGACION = {
   8: { x: -100, y: 1400, scale: 0.9 },  // pioneros artísticos
   9: { x: -100, y: 4000, scale: 0.7 },  // Otros trabajos
   10: { x: 2500, y: 3550, scale: 0.8 }, // Pioneros de lo digital
-  11: { x: 50, y: 8190, scale: 0.9 },  // Libros
+  11: { x: -150, y: 8190, scale: 0.7 },  // Libros
   12: { x: -300, y: 2400, scale: 1 }    // Data Artists
 };
 
